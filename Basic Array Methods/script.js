ae1b1eb28f6d7788bc9140e7df0c42ff8b21a028
Daniyal72 <daniyaldanyo72@gmail.com>
2025-03-18 17:47:23 +0500

    basic methods of array

--- Basic Array Methods/script.js
+++ Basic Array Methods/script.js
@@ -29,7 +29,18 @@ let at = array.at(0)
 
 //  4. Array join()
 // Kya Hai?: Ye method array ke sare elements ko ek string mein mila deta hai, aur aap decide kar sakte hain ke unke beech kis cheez ka separator ho.
-let array1 = ["a", "b", "c", "d", "e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-// let array2 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26];
-let array3 = array1.join(" * ");
+let array3 = array.join(" * ");
 document.getElementById("jion").innerHTML = array3;
+
+
+// 5. Array pop()
+// Kya hai?: Is se array ke aakhri element ko hata dete hain, aur usko return karte hain.
+let  pop = array.pop() //[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26];
+document.getElementById("pop").innerHTML = ` pop jo ha wo last element ko hata dete hain "${pop}"`;
+
+
+// 6. Array push()
+// Kya hai?: Is se aap array ke aakhri mein ek ya zyada elements daal sakte hain.
+let push = array.push("Daniyal");
+let a = document.getElementById("push").innerHTML = `Array ke naya length hai: ${array}`; 
+console.log(a); 
